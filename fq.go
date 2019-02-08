f6d6b890ca80aa3c83813dc38928698aaf91c5ab
Ricardo Geraldes <rgeraldes24@gmail.com>
2019-02-08 00:35:12 +0000

    fq: added tests for newFq

--- fq.go
+++ fq.go
@@ -19,21 +19,23 @@ func bigFromBase10(value string) *big.Int {
 
 type fq [fqLen]uint64
 
+// String satisfies the Stringer interface.
 func (e *fq) String() string {
 	return fmt.Sprintf("%16.16x%16.16x%16.16x%16.16x%16.16x%16.16x", e[5], e[4], e[3], e[2], e[1], e[0])
 }
 
-func newFq(str string) (out *fq) {
+func newFq(str string) *fq {
+	fq := &fq{}
 	words := bigFromBase10(str).Bits()
 	numWords := len(words)
 	if strconv.IntSize == 64 {
-		for i := 0; i < numWords; i++ {
-			out[i] = uint64(words[i])
+		for i := 0; i < numWords && i < fqLen; i++ {
+			fq[i] = uint64(words[i])
 		}
 	} else {
-		for i := 0; i < numWords; i++ {
-			out[i/2] = uint64(words[i]) << uint(32*(i%2))
+		for i := 0; i < numWords && i < fqLen*2; i++ {
+			fq[i/2] = uint64(words[i]) << uint(32*(i%2))
 		}
 	}
-	return
+	return fq
 }
--- fq_test.go
+++ fq_test.go
@@ -1,7 +1,38 @@
 package bls12
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestNewFq(t *testing.T) {
+	testCases := []struct {
+		input  string
+		output string
+	}{
+		{
+			input:  "1",
+			output: "000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001",
+		},
+		{
+			input:  "0",
+			output: "000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+		},
+		{
+			input:  "4002409555221667393417789825735904156556882819939007885332058136124031650490837864442687629129015664037894272559787",
+			output: "1a0111ea397fe69a4b1ba7b6434bacd764774b84f38512bf6730d2a0f6b0f6241eabfffeb153ffffb9feffffffffaaab",
+		},
+		{
+			input:  "40312312312302409555221667393417789825735904156556882819939007885332058136124031650490837864442687629129015664037894272559787",
+			output: "c365b13802f37e3028339cc48d630919b974b3740485f0a23abf8f8762dc16666199fffeb153ffffb9feffffffffaaab",
+		},
+	}
 
+	for _, testCase := range testCases {
+		t.Run(fmt.Sprintf(""), func(t *testing.T) {
+			if fq := newFq(testCase.input); fq.String() != testCase.output {
+				t.Errorf("expected %s, got %s", testCase.output, fq)
+			}
+		})
+	}
 }
