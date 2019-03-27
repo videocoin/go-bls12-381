2d15f6d3ee07ee1524b8f3f783addbac7d164799
Ricardo Geraldes <rgeraldes24@gmail.com>
2019-03-27 18:12:29 +0000

    field: correct version of the exp op

--- fq_generic.go
+++ fq_generic.go
@@ -169,12 +169,12 @@ func FqSqrt(x, a *Fq) bool {
 func FqExp(ret, base *Fq, exponent []uint64) {
 	// See https://www.coursera.org/lecture/mathematical-foundations-cryptography/square-and-multiply-ty62K
 	b := *base
-	*ret := FqMont1
+	*ret = FqMont1
 	// TODO i is not necessary
 	for _, word := range exponent {
 		for j := uint(0); j < wordSize; j++ {
 			if (word & (1 << j)) != 0 {
-				FqMul(&ret, &ret, &b)
+				FqMul(ret, ret, &b)
 			}
 			FqSqr(&b, &b)
 		}
