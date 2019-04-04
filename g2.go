8e367044df2434319c953bb6e7af04b0c33ca3a2
Ricardo Geraldes <rgeraldes24@gmail.com>
2019-04-04 13:54:17 +0100

    g2: included test for method Element

--- g2.go
+++ g2.go
@@ -4,13 +4,30 @@ import "math/big"
 
 var (
 	G2          = new(g2)
-	g2Generator = newTwistPoint(newFq2(g2X0, g2X1), newFq2(g2Y0, g2Y1))
+	g2Generator = &g2Point{newTwistPoint(newFq2(g2X0, g2X1), newFq2(g2Y0, g2Y1))}
 )
 
-type g2Point = twistPoint
+type g2Point struct {
+	p *twistPoint
+}
+
+func newG2Point() *g2Point {
+	return &g2Point{
+		p: new(twistPoint),
+	}
+}
+
+func (z *g2Point) ScalarMult(x *g2Point, scalar *big.Int) *g2Point {
+	z.p.ScalarMult(x.p, scalar)
+	return z
+}
+
+func (z *g2Point) Equal(x *g2Point) bool {
+	return z.p.Equal(x.p)
+}
 
 type g2 struct{}
 
-func (g2 *g2) Element(index *big.Int) *twistPoint {
-	return new(g2Point).ScalarMult(g2Generator, index)
+func (g2 *g2) Element(index *big.Int) *g2Point {
+	return newG2Point().ScalarMult(g2Generator, index)
 }
