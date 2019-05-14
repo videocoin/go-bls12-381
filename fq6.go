654e9edc897a0f8dd41ac66fe269d10d3f83b4bb
Ricardo Geraldes <rgeraldes24@gmail.com>
2019-05-14 11:39:41 -0700

    fq6: added tests for Set

--- fq6.go
+++ fq6.go
@@ -1,8 +1,8 @@
 package bls12
 
 // fq6 is an element of Fq6 = Fq²[Y]/(Y³ − γ), where γ is a quadratic
-// non-residue in Fq and γ = √β is a cubic non-residue in Fq² with a value
-// of X + 1. See https://eprint.iacr.org/2006/471.pdf for arithmetic.
+// non-residue in Fq and γ = √β is a cubic non-residue in Fq² with a value of
+// X + 1. See https://eprint.iacr.org/2006/471.pdf for arithmetic.
 type fq6 struct {
 	c0, c1, c2 fq2
 }
--- fq6_test.go
+++ fq6_test.go
@@ -2,14 +2,81 @@ package bls12
 
 import "testing"
 
-func TestFq6SetZero(t *testing.T) {
-	// TODO
-}
-func TestFq6SetOne(t *testing.T) {
-	// TODO
+func TestFq6Set(t *testing.T) {
+	tests := map[string]struct {
+		input, want fq6
+	}{
+		"0 > input": {
+			input: fq6{},
+			want:  fq6{},
+		},
+		"c0 + c1X + c2X^2 > input": {
+			input: fq6{
+				c0: fq2{
+					c0: fq{0x43F5FFFFFFFCAAAE, 0x32B7FFF2ED47FFFD, 0x7E83A49A2E99D69, 0xECA8F3318332BB7A, 0xEF148D1EA0F4C069, 0x40AB3263EFF0206},
+					c1: fq{0x40AB3263EFF0206, 0xEF148D1EA0F4C069, 0xECA8F3318332BB7A, 0x7E83A49A2E99D69, 0x32B7FFF2ED47FFFD, 0x43F5FFFFFFFCAAAE},
+				},
+				c1: fq2{
+					c0: fq{3},
+					c1: fq{4},
+				},
+				c2: fq2{
+					c0: fq{0xB9FEFFFFFFFFAAAA, 0x1EABFFFEB153FFFF, 0x6730D2A0F6B0F624, 0x64774B84F38512BF, 0x4B1BA7B6434BACD7, 0x1A0111EA397FE69A},
+					c1: fq{6},
+				},
+			},
+			want: fq6{
+				c0: fq2{
+					c0: fq{0x43F5FFFFFFFCAAAE, 0x32B7FFF2ED47FFFD, 0x7E83A49A2E99D69, 0xECA8F3318332BB7A, 0xEF148D1EA0F4C069, 0x40AB3263EFF0206},
+					c1: fq{0x40AB3263EFF0206, 0xEF148D1EA0F4C069, 0xECA8F3318332BB7A, 0x7E83A49A2E99D69, 0x32B7FFF2ED47FFFD, 0x43F5FFFFFFFCAAAE},
+				},
+				c1: fq2{
+					c0: fq{3},
+					c1: fq{4},
+				},
+				c2: fq2{
+					c0: fq{0xB9FEFFFFFFFFAAAA, 0x1EABFFFEB153FFFF, 0x6730D2A0F6B0F624, 0x64774B84F38512BF, 0x4B1BA7B6434BACD7, 0x1A0111EA397FE69A},
+					c1: fq{6},
+				},
+			},
+		},
+	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := new(fq6).Set(&tc.input)
+			if *got != tc.want {
+				t.Fatalf("expected: %v, got: %v", tc.want, got)
+			}
+		})
+	}
 }
 
-func TestFq6Set(t *testing.T) {
+/*
+func TestFq6SetZero(t *testing.T) {
+	tests := map[string]struct {
+		input fq2
+	}{
+		"0 > 0": {
+			input: fq2{},
+		},
+		"c0 + c1X > 0": {
+			input: fq2{
+				c0: fq{0x43F5FFFFFFFCAAAE, 0x32B7FFF2ED47FFFD, 0x7E83A49A2E99D69, 0xECA8F3318332BB7A, 0xEF148D1EA0F4C069, 0x40AB3263EFF0206},
+				c1: fq{0x43F5FFFFFFFCAAAE, 0x32B7FFF2ED47FFFD, 0x7E83A49A2E99D69, 0xECA8F3318332BB7A, 0xEF148D1EA0F4C069, 0x40AB3263EFF0206},
+			},
+		},
+	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := tc.input.SetZero()
+			if (*got != fq2{}) {
+				t.Fatalf("expected: %v, got: %v", fq2{}, got)
+			}
+		})
+	}
+}
+
+func TestFq6SetOne(t *testing.T) {
 	// TODO
 }
 
@@ -44,3 +111,4 @@ func TestFq6Sqr(t *testing.T) {
 func TestFq6Inv(t *testing.T) {
 	// TODO
 }
+*/
